cf5d9db6726d926b8550aff1648cf78daf728a0d
Kushagra Johari <skjkushagra1309@gmail.com>
2021-12-16 17:49:46 +0530

    update title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>The Scheduler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
