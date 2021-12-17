b0eb1d4cfe6608d3880ffb677e01764b6a1ba6ce
Kushagra Johari <skjkushagra1309@gmail.com>
2021-12-17 18:47:41 +0530

    time checker

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,8 @@ export default function Home() {
     setData(data);
   }, []);
 
+  console.log(window);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-2 text-center">
